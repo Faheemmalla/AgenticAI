63e333d54725af78d457efffc6d3a5ac0556ef00
Faheemmalla <faheem.yousuf2004@gmail.com>
2026-07-20 21:45:56 +0530

    Runnables(4)

---
+++ runnable/run_branch.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+from langchain_groq import ChatGroq
+model = ChatGroq(model="qwen/qwen3.6-27b")
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough,RunnableSequence,RunnableParallel,RunnableLambda,RunnableBranch
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
